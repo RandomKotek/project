bd8af356948478e3e6a165bf0cb15ce2cf38b107
RandomKotek <stormyboy78090@gmail.com>
2017-09-01 04:05:48 -0400

    Transform page to React

---
+++ src/components/Images.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class Images extends React.Component {
+    render() {
+        return (
+            <div id="lulz">
+                <a href="https://www.youtube.com/watch?v=5QUZ360iyi0" target="_blank">
+                    <img src="src/img/mem1.jpg"/>
+                </a>
+                <a href="https://www.youtube.com/watch?v=OOQaYdfq-EI" target="_blank">
+                    <img src="src/img/mem2.png"/>
+                </a>
+                <a href="https://www.youtube.com/watch?v=5LTGF2eSTGk" target="_blank">
+                    <img src="src/img/mem3.jpg"/>
+                </a>
+            </div>
+        );
+    }
+}
